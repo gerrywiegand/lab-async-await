3d67d14dc3e9777d47995190e35dea3075e9d1a8
Gerry <gerrywiegand@gmail.com>
2025-09-19 10:42:47 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code here!
+/*fetch("https://jsonplaceholder.typicode.com/todos/")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    displayPosts(data);
+  })
+  .catch((error) => console.error("Error fetching data", error));
+*/
+async function fetchAndDisplay() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/");
+    const data = await response.json();
+    console.log(data);
+    displayPosts(data);
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
+}
+
+fetchAndDisplay();
+
+function displayPosts(posts) {
+  posts.forEach((data) => {
+    const post = document.createElement("li");
+    const title = document.createElement("h1");
+    const list = document.getElementById("post-list");
+    title.textContent = data.title;
+    const body = document.createElement("p");
+    body.textContent = data.body;
+    post.append(title);
+    post.append(body);
+    list.appendChild(post);
+  });
+}
